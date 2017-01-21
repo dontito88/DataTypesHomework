902a06cd80f4d213982d41b7a4f23556b8227565
TrifonEstov <don.tito.g@gmail.com>
2017-01-21 23:01:51 +0200

    New program added!

---
+++ ExactSum/ExactSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactSum
+{
+    class ExactSum
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var inputNumber = decimal.Parse(Console.ReadLine());
+                sum += inputNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
