9254f8d95cd87df009dfa249fae4467edbb2ca88
TrifonEstov <don.tito.g@gmail.com>
2017-01-21 22:54:50 +0200

    Added new program

---
+++ CircleArea/CircleArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            Console.WriteLine("{0:f12}", area);
+        }
+    }
+}
