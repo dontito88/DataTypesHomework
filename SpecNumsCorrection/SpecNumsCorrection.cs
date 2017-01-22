0b6b6587caf72a93dc0a5f2e0ae448b87a570ae0
TrifonEstov <don.tito.g@gmail.com>
2017-01-22 18:42:53 +0200

    Last update of the home work.

---
+++ SpecNumsCorrection/SpecNumsCorrection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecNumsCorrection
+{
+    class SpecNumsCorrection
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int digit = 0;
+                while (digit > 0)
+                {
+                    sumOfDigits += digit % 10;
+                    digit = digit / 10;
+                    bool special = false;
+                    if ((sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11))
+                    {
+                        special = true;
+                    }
+                    Console.WriteLine("{0} -> {0}", num, special);
+                }
+                
+            }
+        }
+    }
+}
